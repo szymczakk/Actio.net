cbab9250c2dbdd7693063388c1fb952df34741df
szymczakk <rakaz666@gmail.com>
2018-02-07 19:43:17 +0100

    add missing property

--- src/Actio.Common/Commands/CreateActivity.cs
+++ src/Actio.Common/Commands/CreateActivity.cs
@@ -6,6 +6,7 @@ namespace Actio.Common.Commands
   {
     public Guid Id { get; set; }
     public Guid UserId { get ; set; }
+    public string Category { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public DateTime CreatedAt { get; set; }
--- src/Actio.Common/Events/ActivityCreated.cs
+++ src/Actio.Common/Events/ActivityCreated.cs
@@ -9,18 +9,20 @@ namespace Actio.Common.Events
 
     }
     public Guid Id { get; }
-    public string Name { get; }
     public Guid UserId { get; }
+    public string Category { get; }
+    public string Name { get; }
     public string Description { get; }
     public DateTime CreatedAt { get; }
 
-    public ActivityCreated(Guid id, Guid userId, string name, string description, DateTime createdAt)
+    public ActivityCreated(Guid id, Guid userId, string category, string name, string description, DateTime createdAt)
     {
       this.CreatedAt = createdAt;
       this.Description = description;
       this.UserId = userId;
       this.Name = name;
       this.Id = id;
+      this.Category = category;
     }
 
   }
